0137feeb9a95fd6f4812193746b8a8058f2fe953
Sofia <sofia.arboleda24@gmail.com>
2025-11-18 19:24:10 -0500

    compilacion con mvn y dependencias

---
+++ src/main/java/co/edu/javeriana/clinicasbackend/ClinicasBackendApplication.java
@@ -0,0 +1,12 @@
+package co.edu.javeriana.clinicasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicasBackendApplication.class, args);
+    }
+}
